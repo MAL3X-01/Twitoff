212c3efca06dd45e4d721b705f20b960f4d09f3c
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-08-20 18:45:12 -0400

    Added models

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
-#from .models import DB
+from .models import DB
 
 
 def create_app():
     '''Create and configure an instance of the Flask application'''
     app = Flask(__name__)
-    #app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite3'
-    # DB.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+    DB.init_app(app)
 
     @app.route('/')
     def root():
