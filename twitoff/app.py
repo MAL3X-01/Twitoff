70df70a1cf45e257ef7c8d28b72cbcce342bcc64
Marvin A Davila <marvind@Marvins-MBP.fios-router.home>
2019-08-21 18:33:43 -0400

    Added prediction model and user input

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,7 @@
 from decouple import config
 from flask import Flask, render_template, request
 from .models import DB, User
+from .twitter import add_or_update_user
 
 
 def create_app():
@@ -9,13 +10,28 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = config('database_url')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['ENV'] = config('ENV')   # Change before deploying
     DB.init_app(app)
 
     @app.route('/')
     def root():
         users = User.query.all()
         return render_template('base.html', title='Home', users=users)
+
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<name>', methods=['GET'])
+    def user(name=None):
+        message = ''
+        name = name or request.values['user_name']
+        try:
+            if request.method == 'POST':
+                add_or_update_user(name)
+                message = 'User {} successfully added!'.format(name)
+            tweets = User.query.filter(User.name == name).one().tweets
+        except Exception as e:
+            message = 'Error adding {}: {}'.format(name, e)
+            tweets = []
+        return render_template('user.html', title=name, tweets=tweets,
+                               message=message)
 
     @app.route('/reset')
     def reset():
